332a68f26b49e96dbcada8e631f9b5cbe2aa8c6b
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-01-28 17:47:44 -0500

    Created Main.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by rhondusmithwick on 1/28/16.
+ *
+ * @author Rhondu Smithwick
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Start the application.
+     *
+     * @param primaryStage the primary stage
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Cell Society");
+        CellSociety cs = new CellSociety();
+        cs.begin();
+        primaryStage.show();
+    }
+}
