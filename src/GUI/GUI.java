8de12d7e80c7647b865634660d4013d18463d3fd
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-02-05 22:06:35 -0500

    GUI.java refactors.

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,31 +1,17 @@
 package GUI;
 
-import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 
-import java.util.List;
-
 public class GUI {
-    private Controls myControls;
-    private GridPane myDisplay;
 
-    // public GUI(CellSociety mySociety, Stage controlStage) {
     public GUI() {
     }
 
     public GridPane init(GridPane display, String resource) {
-    	myDisplay = display;
-        SimulationControl mySimControl = new SimulationControl(myDisplay, resource);
-        myControls = new Controls(mySimControl, resource);
-        addToControlPanel(myDisplay);
-        return myDisplay;
-    }
-
-    private void addToControlPanel(GridPane controlPanel) {
-        List<Node> myControlList = myControls.getControls();
-        for (Node control : myControlList) {
-            controlPanel.getChildren().add(control);
-        }
+        SimulationControl mySimControl = new SimulationControl(display, resource);
+        Controls myControls = new Controls(mySimControl, resource);
+        display.getChildren().addAll(myControls.getControls());
+        return display;
     }
 
 }
