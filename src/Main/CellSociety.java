6617d937bf540b738ccd6d92df27985c37b249ce
BLiborio <bruna.liborio@duke.edu>
2016-02-07 05:53:12 -0500

    CSS heart file

--- src/Main/CellSociety.java
+++ src/Main/CellSociety.java
@@ -22,7 +22,7 @@ class CellSociety {
     /**
      * Stylesheet option.
      */
-    public static final String STYLESHEET = "vivid.css";
+    public static final String STYLESHEET = "heart.css";
 
     /**
      * The empty constructor.
