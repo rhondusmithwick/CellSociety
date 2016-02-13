be4f64de916e01e123fdb47abd642205260bd935
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-02-13 16:55:36 -0500

    Extensive refactors for flexibility.

---
+++ src/Grid/CustomPolygon.java
@@ -0,0 +1,50 @@
+package Grid;
+
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Polygon;
+
+/**
+ * Created by rhondusmithwick on 2/13/16.
+ *
+ * @author Rhondu Smithwick
+ */
+abstract class CustomPolygon extends Polygon {
+    private final double x;
+    private final double y;
+    private final double width;
+    private final double height;
+    Point2D[] points;
+
+    CustomPolygon(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+
+    abstract void createPoints();
+
+    void setPoints() {
+        for (Point2D point : points) {
+            getPoints().addAll(point.getX(), point.getY());
+        }
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+}
--- src/Grid/Triangle.java
+++ src/Grid/Triangle.java
@@ -1,64 +1,30 @@
 package Grid;
 
 import javafx.geometry.Point2D;
-import javafx.scene.shape.Polygon;
 
-class Triangle extends Polygon {
-    private final double x;
-    private final double y;
-    private final double width;
-    private final double height;
+class Triangle extends CustomPolygon {
     private final Triangle prevTriangle;
-    private Point2D point1;
-    private Point2D point2;
-    private Point2D point3;
 
     Triangle(double x, double y, double width, double height,
              Triangle prevTriangle) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height);
+        points = new Point2D[3];
         this.prevTriangle = prevTriangle;
         createPoints();
         setPoints();
     }
 
-    private void createPoints() {
+    @Override
+    void createPoints() {
         if (prevTriangle == null) {
-            point1 = new Point2D(x, y);
-            point2 = new Point2D(x + width / 2, y + height);
-            point3 = new Point2D(x + width, y);
+            points[0] = new Point2D(getX(), getY());
+            points[1] = new Point2D(getX() + getWidth() / 2, getY() + getHeight());
+            points[2] = new Point2D(getX() + getWidth(), getY());
         } else {
-            point1 = prevTriangle.point2;
-            point2 = prevTriangle.point3;
-            point3 = new Point2D(point1.getX() + width, point1.getY());
+            points[0] = prevTriangle.points[1];
+            points[1] = prevTriangle.points[2];
+            points[2] = new Point2D(points[0].getX() + getWidth(), points[0].getY());
         }
     }
 
-    private void setPoints() {
-        getPoints().setAll(
-                point1.getX(), point1.getY(),
-                point2.getX(), point2.getY(),
-                point3.getX(), point3.getY()
-        );
-    }
-
-
-    public double getY() {
-        return y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-
-    public double getHeight() {
-        return height;
-    }
 }
