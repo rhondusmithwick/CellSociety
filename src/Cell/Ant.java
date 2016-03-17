b080b4fa45439841a22881caf722003573b48d68
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-03-16 22:13:30 -0400

    Refactored Antalgorithms.

--- src/Cell/Ant.java
+++ src/Cell/Ant.java
@@ -8,18 +8,6 @@ import Cell.ForagingAntsCell.State;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-    I chose this class as my masterpiece because it was the most challenging class to write. It shows
-    my ability to use lambda expressions to sort and choose maximums. It also demonstrates my ability
-    to give abstract methods away as mentioned in my analysis. See the Ant algorithms class for the
-    mathy functions this class depends on. The reason why I chose to segregate these two (one into what is
-    an essentially static class) is that I want all ants to share those methods: no specific ant
-    has a unique method for that. Plus, it helps keep this class focused and clean.
-    This was also a very different simulation: for all the other simulations, we didn't need an agent class.
-    Here, the agent was so complex that we did, and I think we would have needed one for SugarScape if we
-    had done it.
-    Run Foraging Ants in the drop down to test this.
- */
 
 /**
  * Created by rhondusmithwick on 2/8/16.
@@ -34,8 +22,10 @@ class Ant {
     private boolean hasFoodItem = false;
     private boolean isDead = false;
     private boolean needsToMove = false;
+    private final AntAlgorithms antAlgorithms;
 
     Ant(ForagingAntsCell myCell, double myLifeTime) {
+        antAlgorithms = new AntAlgorithms();
         this.myCell = myCell;
         this.myLifeTime = myLifeTime;
     }
@@ -72,9 +62,9 @@ class Ant {
     private ForagingAntsCell getCellToMoveTo(boolean forwardOnly, boolean searchForFood) {
         List<ForagingAntsCell> locSet = createLocSet(forwardOnly);
         if (searchForFood) {
-            return AntAlgorithms.getNeighborTowardsFood(locSet);
+            return antAlgorithms.getNeighborTowardsFood(locSet);
         } else {
-            return AntAlgorithms.getNeighborTowardsHome(locSet);
+            return antAlgorithms.getNeighborTowardsHome(locSet);
         }
     }
 
--- src/Cell/AntAlgorithms.java
+++ src/Cell/AntAlgorithms.java
@@ -6,33 +6,27 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-/*
-    See Ant.java for explanation.
- */
-
 /**
  * Created by rhondusmithwick on 2/17/16.
  *
  * @author Rhondu Smithwick
  */
-final class AntAlgorithms {
-    private AntAlgorithms() {
-    }
+class AntAlgorithms {
 
-    static ForagingAntsCell getNeighborTowardsHome(List<ForagingAntsCell> locSet) {
+    ForagingAntsCell getNeighborTowardsHome(List<ForagingAntsCell> locSet) {
         return locSet.stream()
                 .max((loc1, loc2) -> Double.compare(loc1.getHomePheromones(), loc2.getHomePheromones()))
                 .get();
     }
 
-    static ForagingAntsCell getNeighborTowardsFood(List<ForagingAntsCell> locSet) {
+    ForagingAntsCell getNeighborTowardsFood(List<ForagingAntsCell> locSet) {
         if (!locSet.isEmpty()) {
             return chooseNeighbor(locSet);
         }
         return null;
     }
 
-    private static ForagingAntsCell chooseNeighbor(List<ForagingAntsCell> locSet) {
+    private ForagingAntsCell chooseNeighbor(List<ForagingAntsCell> locSet) {
         double totalProb = getTotalProbability(locSet);
         double prob = new Random().nextDouble() * totalProb;
         double probTracker = 0;
@@ -50,14 +44,14 @@ final class AntAlgorithms {
     }
 
 
-    private static boolean isRightProbability(double prob, double probTracker, double currProb) {
+    private boolean isRightProbability(double prob, double probTracker, double currProb) {
         return (prob >= probTracker)
                 && (prob <= probTracker + currProb);
     }
 
-    private static double getTotalProbability(List<ForagingAntsCell> locSet) {
+    private double getTotalProbability(List<ForagingAntsCell> locSet) {
         return locSet.stream()
-                .mapToDouble(c -> c.getProbChoice())
+                .mapToDouble(ForagingAntsCell::getProbChoice)
                 .sum();
     }
 }
